37f3d3b871b8200a7767c3be3682125f4a346c73
palarax <ilya.thai1@gmail.com>
2019-07-04 15:36:44 +1000

    added mrcnn files

---
+++ anaconda_tensorflow_gpu.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+## Create anaconda environment with tensorflow gpu
+
+env_name=$1
+
+if [ -z $env_name ];
+then
+	echo "Please provide new environment name"
+	exit
+fi
+
+#https://towardsdatascience.com/tensorflow-gpu-installation-made-easy-use-conda-instead-of-pip-52e5249374bc
+conda create --name $env_name tensorflow-gpu 
