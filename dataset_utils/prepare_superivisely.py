764904a26211c70e78da2576caab4c999cd868ef
palarax <ilya.thai1@gmail.com>
2019-08-02 08:23:32 +1000

    updated superivsely function

--- dataset_utils/prepare_superivisely.py
+++ dataset_utils/prepare_superivisely.py
@@ -1,37 +1,29 @@
 import os
 import json
 
-LABELS = ["pedestrian_bb", "cyclist_bb",
-          "facing_back_bb", "on-call_bb", "on-mobile_bb"]
-
-
-def generate_annotation(annotation, img, aFile):
+def generate_yolo_annotation(annotation, img, labels, new_annot):
     """
     Generate annotation file from json
-    <object-class-id> <center-x> <center-y> <width> <height>
+    yolo annotations
+    img_path x_min,y_min,x_max,y_max,class_id
     """
-    ann_list = []
     annot = json.load(open(annotation))
-    width, height = annot["size"]["width"], annot["size"]["height"]
-
     for obj in annot["objects"]:
         # [[left, top], [right, bottom]]
         # [[x1, y1], [x2, y2]]
-        classId = LABELS.index(obj["classTitle"])
+        class_id = labels.index(obj["classTitle"])
         points = obj["points"]["exterior"]  # list of points
-        info = "%s,%d,%d,%d,%d,%d" % (
-            img, points[0][0], points[0][1], points[1][0], points[1][1], classId)
-        print(info)
-        aFile.write(info + '\n')
+        info = "%s %d,%d,%d,%d,%d" % (
+            img, points[0][0], points[0][1], points[1][0], points[1][1], class_id)
+        new_annot.append(info)
 
 if __name__ == "__main__":
+    labels = ["pedestrian_bb", "cyclist_bb", "on-call_bb", "on-mobile_bb", "facing_back_bb"]
     root = "bbox_transform"
     new_img_dir = "dataset/images"
-    # new_annotations = os.path.join("../dataset", "annotations.txt")
-    
     counter = 0
-    aFile = open(os.path.join("../dataset", "annotations.txt"),'w')
-    for d in ["cyclist", "pedestrians_on_phone", "person_backwords"]:
+    ann_list = []
+    for d in ["cyclist", "pedestrians_on_phone", "person_backwords", "people_mobile"]:
         annotations_dir = os.path.join(root, d, "ann")
         img_dir = os.path.join(root, d, "img")
 
@@ -39,10 +31,11 @@ if __name__ == "__main__":
             img = os.path.join(img_dir, ann[:-5])
             new_img_name = "img_{}.jpg".format(counter)
             # move image
-            os.rename(img, os.path.join("../dataset/images", new_img_name))
+            os.rename(img, os.path.join("dataset/images", new_img_name))
             annotation = os.path.join(annotations_dir, ann)
-            
-            generate_annotation(annotation, os.path.join("dataset/images", new_img_name), aFile)
+            generate_yolo_annotation(annotation, os.path.join("dataset/images", new_img_name), labels, ann_list)
             counter += 1
 
-    aFile.close()
+    with open('dataset/annotations.txt', 'w') as f:
+        for annot in ann_list:
+            f.write(annot + '\n')
