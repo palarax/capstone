5ce98f7e051a1a8ba84b336fdbfc82780a6ab76f
palarax <ilya.thai1@gmail.com>
2019-09-06 00:08:09 +1000

    config update for yolo tests

--- train.py
+++ train.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python
 
 import argparse
-import os
 import numpy as np
 import json
 import logging
@@ -12,18 +11,15 @@ import keras.backend as K
 # import h5py  # for saving model
 from keras.layers import Input, Lambda
 from keras.models import Model
-from keras.optimizers import Adam
-from keras.callbacks import TensorBoard, ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
+from keras.optimizers import Adam, SGD
+from keras.callbacks import TensorBoard, ModelCheckpoint, ReduceLROnPlateau, EarlyStopping, LearningRateScheduler
 
 # YOLO model
-from yolo3.model import preprocess_true_boxes, yolo_body, tiny_yolo_body, yolo_loss
-from yolo3.utils import get_random_data, data_generator_wrapper
-
-from keras.utils import plot_model  # plot model
+from yolo3.model import yolo_body, tiny_yolo_body, yolo_loss
+from yolo3.utils import data_generator_wrapper
 
 
 # Initialize GPU
-from keras.backend.tensorflow_backend import set_session
 gpu_options = tf.GPUOptions(allow_growth=True)
 config = tf.compat.v1.ConfigProto(gpu_options=gpu_options)
 #config.gpu_options.allow_growth = True
@@ -31,10 +27,6 @@ sess = tf.compat.v1.Session(config=config)
 
 # Initialize logger
 logger = None
-
-
-def dummy_loss(y_true, y_pred):
-    return tf.sqrt(tf.reduce_sum(y_pred))
 
 
 def setup_logger(log, filename, loglevel, format, dtfmt):
@@ -97,39 +89,30 @@ def create_model(input_shape, anchors, num_classes, load_pretrained=True, freeze
     return model
 
 
-def create_tiny_model(input_shape, anchors, num_classes, load_pretrained=True, freeze_body=2,
-                      weights_path='model_data/tiny_yolo_weights.h5'):
-    '''create the training model, for Tiny YOLOv3'''
-    K.clear_session()  # get a new session
-    image_input = Input(shape=(None, None, 3))
-    h, w = input_shape
-    num_anchors = len(anchors)
+class PolynomialDecay():
+    def __init__(self, maxEpochs=100, initAlpha=0.01, power=1.0):
+        # store the maximum number of epochs, base learning rate,
+        # and power of the polynomial
+        self.maxEpochs = maxEpochs
+        self.initAlpha = initAlpha
+        self.power = power
 
-    y_true = [Input(shape=(h//{0: 32, 1: 16}[l], w//{0: 32, 1: 16}[l],
-                           num_anchors//2, num_classes+5)) for l in range(2)]
+    def __call__(self, epoch):
+        # compute the new learning rate based on polynomial decay
+        decay = (1 - (epoch / float(self.maxEpochs))) ** self.power
+        alpha = self.initAlpha * decay
 
-    model_body = tiny_yolo_body(image_input, num_anchors//2, num_classes)
-    print('Create Tiny YOLOv3 model with {} anchors and {} classes.'.format(
-        num_anchors, num_classes))
+        # return the new learning rate
+        return float(alpha)
 
-    if load_pretrained:
-        model_body.load_weights(weights_path, by_name=True, skip_mismatch=True)
-        print('Load weights {}.'.format(weights_path))
-        if freeze_body in [1, 2]:
-            # Freeze the darknet body or freeze all but 2 output layers.
-            num = (20, len(model_body.layers)-2)[freeze_body-1]
-            for i in range(num):
-                model_body.layers[i].trainable = False
-            print('Freeze the first {} layers of total {} layers.'.format(
-                num, len(model_body.layers)))
-
-    model_loss = Lambda(yolo_loss, output_shape=(1,), name='yolo_loss',
-                        arguments={'anchors': anchors, 'num_classes': num_classes, 'ignore_thresh': 0.7})(
-        [*model_body.output, *y_true])
-    model = Model([model_body.input, *y_true], model_loss)
-
-    return model
-
+def lr_schedule(epoch):
+    if epoch < 5:
+        return 0.001
+    elif epoch < 10:
+        return 0.0001
+    else:
+        return 0.00001
+		
 
 def _main_(args):
 
@@ -182,12 +165,8 @@ def _main_(args):
     # input_shape = (416, 416)  # multiple of 32, hw
     input_shape = (608, 608)
 
-    is_tiny_version = len(anchors) == 6  # default setting
-    if is_tiny_version:
-        model = create_tiny_model(input_shape, anchors, num_classes,
-                                  freeze_body=2, weights_path=config["model"]["weights"])  # 'model_data/tiny_yolo_weights.h5'
-    else:
-        model = create_model(input_shape, anchors, num_classes,
+    model = create_model(input_shape, anchors, num_classes,
+                                # freeze_body=2, weights_path="model_stages/004/trained_model_stage_1.h5")
                              freeze_body=2, weights_path=config["model"]["weights"])  # make sure you know what you freeze
 
     ###############################
@@ -198,8 +177,19 @@ def _main_(args):
     tensorboard = TensorBoard(log_dir=config["train"]["tensorboard_dir"])
     checkpoint = ModelCheckpoint(config["train"]["model_stages"] + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
                                  monitor='val_loss', save_weights_only=False, save_best_only=True, period=5)
-    reduce_lr = ReduceLROnPlateau(
-        monitor='val_loss', factor=0.01, patience=3, verbose=1)
+    # reduce_lr = ReduceLROnPlateau(
+    #     monitor='val_loss', factor=0.01, patience=2, verbose=1)
+
+    # reduce_lr = ReduceLROnPlateau(
+    #     monitor='val_loss', factor=0.001, patience=2, verbose=1)
+
+    schedule = PolynomialDecay(maxEpochs=config["train"]["epochs"], initAlpha=1e-3, power=2)  # TODO
+    # # schedule = lr_schedule
+    learning_rate_scheduler = LearningRateScheduler(schedule=schedule, verbose=1)
+    # optim = Adam(lr=1e-3)
+    optim = Adam(lr=1e-3, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+    # optim = SGD(lr=1e-4, momentum=0.9, decay=0.0, nesterov=False)
+
     early_stopping = EarlyStopping(
         monitor='val_loss', min_delta=0, patience=10, verbose=1)
 
@@ -209,10 +199,9 @@ def _main_(args):
     print("[INFO] Start Training")
     # Train with frozen layers first, to get a stable loss.
     # Adjust num epochs to your dataset. This step is enough to obtain a not bad model.
-    optz = Adam(lr=1e-3)
 
     if True:
-        model.compile(optimizer=optz, loss={
+        model.compile(optimizer=optim, loss={
             # use custom yolo_loss Lambda layer.
             'yolo_loss': lambda y_true, y_pred: y_pred},
             metrics=['mean_squared_error'])
@@ -228,26 +217,29 @@ def _main_(args):
                             validation_steps=max(1, len(val)//batch_size),
                             initial_epoch=0,
                             epochs=config["train"]["epochs"],
-                            callbacks=[tensorboard, checkpoint])
+                            callbacks=[tensorboard,learning_rate_scheduler, checkpoint])
         # model.save_weights(log_dir + 'trained_weights_stage_1.h5')
         model.save_weights(config["train"]["model_stages"] +
                            'trained_model_stage_1.h5')
 
-    optz = Adam(lr=1e-4)
+    schedule = PolynomialDecay(maxEpochs=config["train"]["epochs_unfrozen"], initAlpha=1e-4, power=2)
+    learning_rate_scheduler = LearningRateScheduler(schedule=schedule, verbose=1)
+    optim = Adam(lr=1e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+    # optim = SGD(lr=1e-5, momentum=0.9, decay=0.0, nesterov=False)
     # Unfreeze and continue training, to fine-tune.
     # Train longer if the result is not good.
     if True:
         for i in range(len(model.layers)):
             model.layers[i].trainable = True
         # recompile to apply the change
-        model.compile(optimizer=optz, loss={
+        model.compile(optimizer=optim, loss={
             # use custom yolo_loss Lambda layer.
             'yolo_loss': lambda y_true, y_pred: y_pred},
             metrics=['mean_squared_error'])
 
         print('[INFO] Unfreeze all of the layers.')
 
-        batch_size = 2  # note that more GPU memory is required after unfreezing the body
+        batch_size = 1  # note that more GPU memory is required after unfreezing the body
         print('[INFO] Train on {} samples, val on {} samples, with batch size {}.'.format(
             len(train), len(val), batch_size))
         model.fit_generator(data_generator_wrapper(train, batch_size, input_shape, anchors, num_classes),
@@ -256,8 +248,9 @@ def _main_(args):
                                 val, batch_size, input_shape, anchors, num_classes),
                             validation_steps=max(1, len(val)//batch_size),
                             initial_epoch=config["train"]["epochs"],
-                            epochs=config["train"]["epochs"]+10,
-                            callbacks=[tensorboard, checkpoint, reduce_lr, early_stopping])
+                            epochs=config["train"]["epochs_unfrozen"],
+                            callbacks=[tensorboard, checkpoint, learning_rate_scheduler, early_stopping])
+                            # callbacks=[tensorboard, checkpoint, reduce_lr, early_stopping])
         model.save_weights(config["train"]["model_stages"] +
                            'trained_model_stage_2.h5')
 
