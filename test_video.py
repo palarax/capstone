7fb64274eba2e79f84f7cb424b9f8c66f13cc865
palarax <ilya.thai1@gmail.com>
2019-09-07 21:33:34 +1000

    module seperation

--- test_video.py
+++ test_video.py
@@ -27,20 +27,14 @@ from XAI.ssd_encoder_decoder.ssd_output_decoder import decode_detections_fast
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 tf.Session(config=tf.ConfigProto(log_device_placement=True))
 
-def load_ssd_model(model_path="SSD_MODEL.h5"):
-    '''Load Pretrained SSD model
+def setup_log(log_config):
+    '''Setup logging
     '''
-    logging.info(f"Loading SSD model [{model_path}]")
-    # We need to create an SSDLoss object in order to pass that to the model loader.
-    ssd_loss = SSDLoss(neg_pos_ratio=3, n_neg_min=0, alpha=1.0)
-    K.clear_session()  # Clear previous models from memory.
-    model = load_model(model_path, custom_objects={'AnchorBoxes': AnchorBoxes,
-                                                   'L2Normalization': L2Normalization,
-                                                   'DecodeDetections': DecodeDetections,
-                                                   'compute_loss': ssd_loss.compute_loss})                                     
-    return model
+    with open(log_config, encoding='utf-8-sig') as conf_file:
+        jc = json.load(conf_file)
+        logging.config.dictConfig(jc)
 
-def get_fps(accum_time, curr_fps, prev_time, fps):
+def calculate_fps(accum_time, curr_fps, prev_time, fps, image):
     '''Calculate current FPS
     '''
     curr_time = timer()
@@ -52,68 +46,102 @@ def get_fps(accum_time, curr_fps, prev_time, fps):
         accum_time = accum_time - 1
         fps = "FPS: " + str(curr_fps)
         curr_fps = 0
+    # Draw FPS in top left corner
+    cv2.rectangle(image, (0, 0), (50, 17), (255, 255, 255), -1)
+    cv2.putText(image, fps, (3, 10),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 0), 1)
     return accum_time, curr_fps, prev_time, fps
 
-def detect_image(model, frame, vidw, vidh):
-    img_height = 300
-    img_width = 300
-    class_colors = [(0, 0, 0), (0, 255, 0), (0, 0, 255)]
-    classes = ['background', 'cyclist', 'risk']
 
+def get_color(classes):
+    # TODO: implement through DB
+    # ["background", "cyclist", "risk"],
+    # BGR colors
+    BACKGROUND = (0, 0, 0)
+    DANGER = (0, 0, 255)  # RED
+    WARNING = (0, 128, 255) # ORANGE
+    CAUTION = (0, 255, 255) # YELLOW
+    NO_IMMEDIATE_DANGER = (255, 0, 0) # BLUE
+    return [(0, 0, 0), DANGER, WARNING]
+
+def load_ssd_model(model_path="SSD_MODEL.h5"):
+    '''Load Pretrained SSD model
+    '''
+    logging.info("Loading SSD model %s", model_path)
+    # We need to create an SSDLoss object in order to pass that to the model loader.
+    ssd_loss = SSDLoss(neg_pos_ratio=3, n_neg_min=0, alpha=1.0)
+    K.clear_session()  # Clear previous models from memory.
+    model = load_model(model_path, custom_objects={'AnchorBoxes': AnchorBoxes,
+                                                   'L2Normalization': L2Normalization,
+                                                   'DecodeDetections': DecodeDetections,
+                                                   'compute_loss': ssd_loss.compute_loss})
+    return model
+
+def get_prediction(model, frame, confidence_thresh, iou_threshold, img_height=300, img_width=300):
+    '''Get model prediction
+    return decoded prediction
+    '''
     img = cv2.resize(frame, (img_height, img_width))
     inp_img = [image.img_to_array(img)]
     tmp_inp = np.array(inp_img)
     preprocessed_inp = preprocess_input(tmp_inp)
-    y_pred = model.predict(preprocessed_inp)
+    prediction = model.predict(preprocessed_inp)
+    return decode_detections_fast(prediction,
+                                  confidence_thresh=confidence_thresh,
+                                  iou_threshold=iou_threshold,
+                                  top_k=200,
+                                  normalize_coords=True,
+                                  img_height=img_height,
+                                  img_width=img_width)
 
-    y_pred_decoded = decode_detections_fast(y_pred,
-                                            confidence_thresh=0.5,
-                                            iou_threshold=0.4,
-                                            top_k=200,
-                                            normalize_coords=True,
-                                            img_height=img_height,
-                                            img_width=img_width)
-
+def draw_objects(prediction, frame, classes, img_height=300, img_width=300):
+    '''Draw objects in image frame
+    '''
+    class_colors = [[0, 0, 0],[0, 128, 255],[0, 0, 255]]
     height, width, _ = frame.shape
 
-    for box in y_pred_decoded[0]:
+    for obj in prediction[0]:
         # Transform the predicted bounding boxes for the 300x300 image to the original image dimensions.
         # [0]class   [1]conf  [2]xmin   [3]ymin   [4]xmax   [5]ymax
-        label = '{}: {:.2f}'.format(classes[int(box[0])], box[1])
-        conf = int(box[1])
+        label = '{}: {:.2f}'.format(classes[int(obj[0])], obj[1])
+        conf = int(obj[1])
 
-        xmin = int(round(box[2] * width / img_width))
-        ymin = int(round(box[3] * height / img_height))
-        xmax = int(round(box[4] * width / img_width))
-        ymax = int(round(box[5] * height / img_height))
+        xmin = int(round(obj[2] * width / img_width))
+        ymin = int(round(obj[3] * height / img_height))
+        xmax = int(round(obj[4] * width / img_width))
+        ymax = int(round(obj[5] * height / img_height))
 
         cv2.rectangle(frame, (xmin, ymin), (xmax, ymax),
-                      class_colors[int(box[0])], 3)
+                      class_colors[int(obj[0])], 3)
 
         text_top = (xmin, ymin-10)
         text_bot = (xmin + 80, ymin + 5)
         text_pos = (xmin + 5, ymin)
-        cv2.rectangle(frame, text_top, text_bot, class_colors[int(box[0])], -1)
+        cv2.rectangle(frame, text_top, text_bot, class_colors[int(obj[0])], -1)
         cv2.putText(frame, label, text_pos,
                     cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 0), 1)
-
     return frame
 
+def process_video(model, config, video_path=0):
+    '''Process video or camera stream
+    video_path: file or (0 or -1) for video stream
+    '''
+    confidence_thresh = config["confidence_thresh"]
+    iou_threshold = config["iou_threshold"]
+    img_height = config["img_height"]
+    img_width = config["img_width"]
+    class_labels = config["labels"]
 
-def detect_video(model, video_path, output_path="output"):
+    # Setup values to calculate FPS
+    accum_time, curr_fps = 0, 0
+    fps = "FPS: ??"
+    prev_time = timer()
+
     vid = cv2.VideoCapture(video_path)
-
     if not vid.isOpened():
         raise IOError("Couldn't open webcam or video")
 
     vid.set(cv2.CAP_PROP_FPS, 30)
-    vidw = vid.get(cv2.CAP_PROP_FRAME_WIDTH)
-    vidh = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
-
-    accum_time = 0
-    curr_fps = 0
-    fps = "FPS: ??"
-    prev_time = timer()
 
     while True:
         return_value, frame = vid.read()
@@ -121,13 +149,17 @@ def detect_video(model, video_path, output_path="output"):
         if not return_value:
             break
 
-        image = detect_image(model, frame, vidw, vidh)
+        predictions = get_prediction(
+            model, frame, confidence_thresh, iou_threshold, img_height, img_width)
+        
+        # TODO: implement IoU
+        # TODO: implement risk analysis
+        image = draw_objects(
+            predictions, frame, class_labels, img_height, img_width)
 
-        accum_time, curr_fps, prev_time, fps = get_fps(accum_time, curr_fps, prev_time, fps)
-        # Draw FPS in top left corner
-        cv2.rectangle(image, (0, 0), (50, 17), (255, 255, 255), -1)
-        cv2.putText(image, fps, (3, 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 0), 1)
+        accum_time, curr_fps, prev_time, fps = calculate_fps(
+            accum_time, curr_fps, prev_time, fps, image)
+
         cv2.imshow("SSD results", image)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -137,19 +169,17 @@ def detect_video(model, video_path, output_path="output"):
     cv2.destroyAllWindows()
 
 
-def main(config_file="configuration/log_config.json"):
+def main(log_config="configuration/log_config.json", main_config="configuration/config.json"):
     """ Main Function
     """
-    with open(config_file, encoding='utf-8-sig') as json_config:
-        config = json.load(json_config)
-        logging.config.dictConfig(config)
+    setup_log(log_config)
+    config = {}
+    with open(main_config, encoding='utf-8-sig') as conf_file:
+        config = json.load(conf_file)
+    model = load_ssd_model(config["model_processing"]["file"])
 
-    with open(config_file, encoding='utf-8-sig') as json_config:
-        config = json.load(json_config)
-        
-    model = load_ssd_model()
-    detect_video(model, "dataset/test.mp4")
+    process_video(model, config["model_processing"], config["video_path"])
+
 
 if __name__ == "__main__":
     main()
-
